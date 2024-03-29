4f35b12e721b54c675245092f661581837427032
wpineth <aneth@wpi.edu>
2024-03-29 12:15:17 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const FS = require('fs');
+
+const Discord = require('discord.js');
+
+const client = new Discord.Client({
+    intents: [
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.MessageContent,
+        Discord.GatewayIntentBits.DirectMessages
+    ],
+    partials: [
+      Discord.Partials.Channel,
+      Discord.Partials.Message
+    ]
+});
+
+function relay(channel, message){
+    channel.send(message);
+}
+
+const config = JSON.parse(FS.readFileSync('data/config.json'));
+
+client.once(Discord.Events.ClientReady, () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on(Discord.Events.MessageCreate, (message) => {
+    if(message.author.id !== client.user.id){
+        if(message.channel.type === Discord.ChannelType.DM){
+            client.guilds.fetch(config.guild_id).then((guild) => {
+                guild.channels.fetch().then((channels) => {
+                    const channel = Array.from(channels.filter((channel) => channel.name === message.author.id).values())[0];
+
+                    if(channel === undefined){
+                        guild.channels.create({
+                            name: message.author.id,
+                            type: Discord.ChannelType.GuildText,
+                            parent: config.category_id,
+                        }).then((channel) => {
+                            relay(channel, message.content);
+                        });
+                    }else{
+                        relay(channel, message.content);
+                    }
+                });
+            });
+        }else{
+            client.users.fetch(message.channel.name).then((user) => {
+                user.send(message.content).catch((err) => {
+                    console.log(err);
+                });
+            });
+        }
+    }
+});
+
+client.login(config.token);
